d63102a8d7015099f12ae714d7ef0f5fc8bab837
JoshSong <Joshua_Song@outlook.com>
2018-07-12 23:50:12 +1000

    Flickr32 eval

---
+++ research/flickr32_infer/infer.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=c4130,c4130-s
+source /home/Staff/uqjsong2/tensorflow/bin/activate
+start=`date +%s`
+#TF_RECORD_FILES=/home/Staff/uqjsong2/data/detpri_real.record
+#TF_RECORD_FILES=/home/Staff/uqjsong2/data/test/hypo_0_test.record
+TF_RECORD_FILES=/home/Staff/uqjsong2/data/test/flickrlogos_32_test.record
+#TF_RECORD_FILES=/home/Staff/uqjsong2/data/test/output_7_test.record
+PYTHONPATH=$PYTHONPATH:$(readlink -f ..) \
+python -m object_detection.inference.infer_detections \
+  --input_tfrecord_paths=$TF_RECORD_FILES \
+  --output_tfrecord_path=detections.tfrecord \
+  --discard_image_pixels \
+  --inference_graph=/home/Staff/uqjsong2/export_dir38/frozen_inference_graph.pb
+end=`date +%s`
+runtime=$((end-start))
+echo "Done. Took ${runtime} seconds"
+
+echo "Running metrics"
+
+mkdir -p eval_metrics
+
+#label_map_path: '/home/Staff/uqjsong2/tfresearch/research/synthetic/detpri_label_map.pbtxt'
+echo "
+label_map_path: '../object_detection/data/flickr32_label_map.pbtxt'
+tf_record_input_reader: { input_path: 'detections.tfrecord' }
+" > eval_metrics/input_config.pbtxt
+
+#echo "
+#metrics_set: 'open_images_metrics'
+#" > eval_metrics/eval_config.pbtxt
+
+PYTHONPATH=$PYTHONPATH:$(readlink -f ..) \
+python -m object_detection.metrics.offline_eval_map_corloc \
+  --eval_dir=eval_metrics \
+  --eval_config_path=eval_metrics/eval_config.pbtxt \
+  --input_config_path=eval_metrics/input_config.pbtxt
