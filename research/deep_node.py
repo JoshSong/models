0b4cdef61b78ed05879102b659281d30caf39021
JoshSong <Joshua_Song@outlook.com>
2018-08-16 15:25:52 +1000

    Deep node updates

--- research/deep_node.py
+++ research/deep_node.py
@@ -7,23 +7,42 @@ import rospy
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from std_srvs.srv import Trigger, TriggerResponse
+from std_msgs.msg import Int32
 from sensor_msgs.msg import Image
 
 import tensorflow as tf
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
+width = 480
+height = 360
+#width = 960
+#height = 720
+num_scans = 10
+
+saved_count = 0
+
 bridge = CvBridge()
+cv_image = None
+image_sub = None
 image_pub = None
+int_pub = None
 
 detection_graph = None
 sess = None
 category_index = None
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+OUTPUT_DIR = os.path.join(FILE_DIR, 'deep_node_out')
 
 def detect_objects(image_np):
     global detection_graph, sess, category_index
+
+    # Crop image
+    row_off = (image_np.shape[0]-height)/2
+    col_off = (image_np.shape[1]-width)/2
+    image_np = image_np[row_off:row_off+height, col_off:col_off+width]
+
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
@@ -50,37 +69,69 @@ def detect_objects(image_np):
         np.squeeze(scores),
         category_index,
         use_normalized_coordinates=True,
-        min_score_thresh=0.3,
+        min_score_thresh=0.2,
         line_thickness=5)
+
     return image_np, scores, classes
 
 def ros_service(req):
+    global cv_image, saved_count
     print 'called'
-    global video_capture, detection_graph, sess
-    frame = video_capture.read()
-    output_img, scores, classes = detect_objects(frame)
-    success = scores[0][0] > 0.1
-    #cv2.imshow('tensorflow', output_img)
-    #cv2.waitKey(5000)
-    #cv2.destroyAllWindows()
 
-    return TriggerResponse(success=success, message=str(scores) + '\n' + str(classes))
+    num_detections = [0, 0]
+    avg_scores = [0, 0]
+    for i in range(num_scans):
+
+        # Wait for new image
+        cv_image = None
+        while (cv_image is None):
+            rospy.sleep(0.5)
+
+        # Run deep classifier
+        output_img, scores, classes = detect_objects(cv_image)
+
+        # Save visualization image
+        cv2.imwrite(os.path.join(OUTPUT_DIR, str(saved_count) + '.jpg'), output_img)
+        saved_count += 1
+
+        # Just take the top detection
+        score = scores[0][0]
+        logo = classes[0][0]
+        avg_scores[logo - 1] += score
+        num_detections[logo - 1] += 1
+
+    for i in range(len(avg_scores)):
+        if num_detections[i] > 0:
+            avg_scores[i] /= num_detections[i]
+
+    ret = 0
+    if max(avg_scores[1], avg_scores[0]) > 0.1:
+        if avg_scores[1] > avg_scores[0]:
+            ret = 2
+        else:
+            ret = 1
+
+    return TriggerResponse(success=True, message=str(ret))
 
 def callback(data):
+    global cv_image, int_pub
     try:
         cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
         print(e)
 
     output_img, scores, classes = detect_objects(cv_image)
+    print scores
+    print classes
+
+    logo = 0
+    if scores[0][0] > 0.1:
+        logo = classes[0][0]
+    logo = 5
+    int_pub.publish(logo)
 
     cv2.imshow("Image window", output_img)
-    cv2.waitKey(3)
-
-    try:
-        image_pub.publish(bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-        print(e)
+    cv2.waitKey(30)
 
 def init_deep():
     global detection_graph, sess, category_index
@@ -121,14 +172,18 @@ def init_deep():
         sess = tf.Session(graph=detection_graph, config=config)
 
 def main():
-    global image_pub
+    global image_sub, image_pub, int_pub
+
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
 
     init_deep()
 
     rospy.init_node('tensorflow')
-    s = rospy.Service('tensorflow_srv', Trigger, ros_service)
+    s = rospy.Service('/tensorflow_srv', Trigger, ros_service)
 
-    image_pub = rospy.Publisher("image_topic_2", Image, queue_size=1)
+    int_pub = rospy.Publisher("/tensorflow_msg", Int32, queue_size=1)
+    #image_pub = rospy.Publisher("image_topic_2", Image, queue_size=1)
     image_sub = rospy.Subscriber("/kinect2/hd/image_color", Image, callback)
     rospy.spin()
 
